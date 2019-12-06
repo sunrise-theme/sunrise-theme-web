25eac6ca0e06f5166d2c05a08f17f1f52c4e2660
Seingshin Lee <seingshinlee@gmail.com>
2019-12-06 16:17:09 +0800

    Add slider and carousel of bright theme

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import Glide from "@glidejs/glide";
 
-new Glide('.glide', {
+new Glide('.dark-glide', {
+    type: 'carousel',
+    autoplay: 1500,
+    animationDuration: 1000
+}).mount();
+
+new Glide('.bright-glide', {
     type: 'carousel',
     autoplay: 1500,
     animationDuration: 1000
