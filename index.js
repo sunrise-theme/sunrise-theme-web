3860dc142c2af95d74a490e8de531ecbde61e6af
Seingshin Lee <seingshinlee@gmail.com>
2020-08-29 11:38:23 +0800

    Update: Ready to simplify recurring boxes <div>

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+
 import Glide from "@glidejs/glide";
+
+function symbolToChar(content) {
+    content = content.replace(/&/g, '&amp;');
+    content = content.replace(/>/g, '&gt;');
+    content = content.replace(/</g, '&lt;');
+    content = content.replace(/'/g, '&apos;');
+    content = content.replace(/"/g, '&quot;');
+    content = content.replace(/ /g, '&nbsp;');
+    return content;
+}
 
 new Glide('.dark-glide', {
     type: 'carousel',
@@ -11,3 +22,9 @@ new Glide('.bright-glide', {
     autoplay: 1000,
     animationDuration: 1000
 }).mount();
+
+document.write("<script type='text/javascript' src='./dom/dom-left-left.js'></script>");
+document.write("<script type='text/javascript' src='./dom/dom-left-right.js'></script>");
+document.write("<script type='text/javascript' src='./dom/dom-right.js'></script>");
+document.write("<script type='text/javascript' src='./dom/dom-bottom-normal.js'></script>");
+document.write("<script type='text/javascript' src='./dom/dom-bottom-bright.js'></script>");
